3cc1260f8f9706a0af371eacced157dcc362b2ef
Rahul-Bisht <142198664+rahul-bisht-dev@users.noreply.github.com>
2023-08-16 06:08:32 +0000

    program3

---
+++ subtractFunction.java
@@ -0,0 +1,9 @@
+public class subtractFunction {
+    
+    static int subtractMethod(int a,int b){return a-b;}
+
+    public static void main(String[] args)
+    {
+        System.out.println(subtractMethod(66,90));
+    }
+}
