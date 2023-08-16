629599fd8c01ae0b9e42219e21168e005480b5fa
Rahul-Bisht <142198664+rahul-bisht-dev@users.noreply.github.com>
2023-08-16 05:54:41 +0000

    program2

---
+++ addFunction.java
@@ -0,0 +1,15 @@
+// this program has function called addMethod 
+// which will return sum of two numbers
+
+class addFunction
+{
+     static int addMethod(int a , int b)
+    {
+        return a+b;
+    }
+
+    public static void main(String[] args)
+    {
+      System.out.println(addMethod(7,100));
+    }
+}
